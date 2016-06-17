9a0f1ef2266791818ac5b411ec0f1c289b00deae
Jesse Dunlap <jesse.dunlap@monsanto.com>
2016-06-17 11:34:27 -0500

    Added library generation abilities.

---
+++ generators/app/templates/webpack.config.library.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'components', 'App.jsx'),
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    library: '<%= names.pascal %>',
+    libraryTarget: 'umd',
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: { presets: ['react', 'es2015'] }
+      }
+    ]
+  }
+}
